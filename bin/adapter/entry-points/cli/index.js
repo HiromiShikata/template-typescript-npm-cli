57c9c6001c599236f5b030b3440560187a962ef9
gh-actions <gh-actions>
2024-05-11 13:40:36 +0000

    autogen: release

--- bin/adapter/entry-points/cli/index.js
+++ bin/adapter/entry-points/cli/index.js
@@ -4,9 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
 const program = new commander_1.Command();
 program
-    .argument('<path>', 'Path of domain entity directory')
-    .name('Get entity definitions')
-    .description('Get entity definitions and relation definitions from types of TypeScript in src directory')
+    .argument('<path>', 'Path of example')
+    .name('Example CLI')
+    .description('This is an example')
     .action(async (path) => {
     console.log(JSON.stringify(path));
 });
